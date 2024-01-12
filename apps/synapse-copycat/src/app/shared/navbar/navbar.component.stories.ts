768bf876a35084fc772baf1c89c35add687d8999
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-11 22:22:44 -0500

    test: update stories

--- apps/synapse-copycat/src/app/shared/navbar/navbar.component.stories.ts
+++ apps/synapse-copycat/src/app/shared/navbar/navbar.component.stories.ts
@@ -3,8 +3,6 @@ import { Device, UsbDeviceSchema } from '../../models';
 
 import { NavbarComponent } from './navbar.component';
 
-import UsbDeviceRepository from '../../../assets/usb-devices.json';
-
 import { z } from 'zod';
 
 export const UsbDeviceRepositorySchema = z.object({
@@ -13,76 +11,6 @@ export const UsbDeviceRepositorySchema = z.object({
   kind: UsbDeviceSchema,
   id: z.string(),
 });
-
-const DeviceRepository = z.record(z.record(UsbDeviceRepositorySchema));
-type DeviceRepository = z.infer<typeof DeviceRepository>;
-
-const t = {
-  '123': {
-    a: {
-      a1: 1,
-      a2: 2,
-    },
-  },
-  '456': {
-    b: {
-      b1: 2,
-      b2: 3,
-    },
-  },
-};
-
-
-type T1A = keyof (typeof t)['123']['a'];
-type T1B = keyof (typeof t)['456']['b'];
-
-const t2 = UsbDeviceRepository
-
-type T2A = keyof (typeof t2)['5426']['134'];
-type T2B = keyof (typeof t2)['5426']['136'];
-
-type ExtractGroupKeys<T> = keyof T;
-type ExtractIdKeys<
-  T extends Record<string, any>,
-  G extends string
-> = keyof T[G];
-
-type T1 = ExtractGroupKeys<typeof t>;
-type T2 = ExtractIdKeys<typeof t, ExtractGroupKeys<typeof t>>;
-
-const extractUsbDevice = <
-  T extends Record<string, any>,
-  G extends string & ExtractGroupKeys<T>,
-  D extends ExtractIdKeys<T, G>
->(
-  devices: T,
-  deviceGroup: keyof T,
-  deviceId: D
-): Device => {
-  const groupExists = !!devices[deviceGroup];
-  if (!groupExists)
-    throw new Error(`Unkown device group ${String(deviceGroup)}`);
-
-  const deviceIdExists = groupExists && !!devices[deviceGroup][deviceId];
-  if (deviceIdExists)
-    throw new Error(
-      `Unknown device id ${String(deviceId)} in device group ${String(
-        deviceGroup
-      )}`
-    );
-
-  const device = devices[deviceGroup][deviceId];
-  return {
-    __type: 'device',
-    group: 'usb',
-
-    id: deviceId,
-
-    name: device.name,
-    kind: device.kind,
-    visual: device.visual,
-  } as Device;
-};
 
 //const a = extractUsbDevice(t, '', '453');
 
@@ -98,7 +26,7 @@ const usbDevices: Device[] = [
   {
     __type: 'device',
     group: 'usb',
-    kind: 'camera',
+    kind: 'streaming',
     name: 'Razer Kiyo',
     visual: './assets/images/razer-logo.svg',
     id: '3202',
