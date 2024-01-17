ff1b3f4357ffe0bcb6158413cf86d1d74f4b5ef7
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-16 22:05:48 -0500

    feat(synapse-ui): create checkbox  component

---
+++ libs/synapse-ui/src/lib/checkbox/checkbox.component.stories.ts
@@ -0,0 +1,25 @@
+
+import { Meta, StoryObj } from '@storybook/angular';
+
+import { CheckboxComponent } from './checkbox.component';
+
+const meta: Meta<CheckboxComponent> = {
+  title: 'Synapse UI / Checkbox',
+  component: CheckboxComponent,
+  tags: ['autodocs'],
+ 
+};
+
+export default meta;
+type Story = StoryObj<CheckboxComponent>;
+
+export const Control: Story = {
+};
+
+
+export const Disabled: Story = {
+  args: {
+    disabled: true
+  }
+};
+
