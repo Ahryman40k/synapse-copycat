ecc440e80f2956ac1e90c6583199758289db25be
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-16 22:08:02 -0500

    feat(synapse): add story to accessory-page component

---
+++ apps/synapse-copycat/src/app/pages/accessory-page/accessory-page.component.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/angular';
+
+import { AccessoryPageComponent } from './accessory-page.component';
+
+const meta: Meta<AccessoryPageComponent> = {
+  title: 'Pages / Accessory Page',
+  component: AccessoryPageComponent,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<AccessoryPageComponent>;
+
+export const DefaultPage: Story = {
+};
+
