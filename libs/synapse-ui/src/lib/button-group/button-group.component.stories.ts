dfe3ac00b93efd1519770d91a199ed90d18c35df
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-16 22:04:59 -0500

    feat(synapse-ui): create button group component

---
+++ libs/synapse-ui/src/lib/button-group/button-group.component.stories.ts
@@ -0,0 +1,25 @@
+
+import { Meta, StoryObj } from '@storybook/angular';
+
+import { ButtonGroupComponent } from './button-group.component';
+
+const meta: Meta<ButtonGroupComponent> = {
+  title: 'Synapse UI / Button Group',
+  component: ButtonGroupComponent,
+  tags: ['autodocs'],
+ 
+};
+
+export default meta;
+type Story = StoryObj<ButtonGroupComponent>;
+
+export const Control: Story = {
+};
+
+
+export const Disabled: Story = {
+  args: {
+    disabled: true
+  }
+};
+
---
+++ libs/synapse-ui/src/lib/button-group/button-group.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'synapse-button-group',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button-group.component.html',
+  styleUrl: './button-group.component.scss',
+})
+export class ButtonGroupComponent {}
