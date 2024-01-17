c6fc7aef5152d7b21e9de755ff66a9300950a28a
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-16 22:08:38 -0500

    feat(synapse): create keyboard page

---
+++ apps/synapse-copycat/src/app/pages/keyboard-page/keyboard-page.component.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/angular';
+
+import { KeyboardPageComponent } from './keyboard-page.component';
+
+const meta: Meta<KeyboardPageComponent> = {
+  title: 'Pages / Keyboard Page',
+  component: KeyboardPageComponent,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<KeyboardPageComponent>;
+
+export const DefaultPage: Story = {
+};
+
