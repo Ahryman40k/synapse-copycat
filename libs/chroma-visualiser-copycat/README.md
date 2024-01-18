1fc984fd488d644fe4cb2693f248c3f573e60e3a
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-18 18:17:08 -0500

    feat(chroma-visualizer): create library

---
+++ libs/chroma-visualiser-copycat/README.md
@@ -0,0 +1,7 @@
+# chroma-visualiser-copycat
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test chroma-visualiser-copycat` to execute the unit tests.
---
+++ libs/chroma-visualiser-copycat/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { ChromaVisualiserCopycatComponent } from './chroma-visualiser-copycat/chroma-visualiser-copycat.component';
+
+export const chromaVisualiserCopycatRoutes: Route[] = [
+  { path: '', component: ChromaVisualiserCopycatComponent },
+];
