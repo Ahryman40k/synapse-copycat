303d40c6795424079162ad625a0c84e131835b24
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-18 16:03:28 -0500

    feat(synapse-copycat): update brightness control component

--- apps/synapse-copycat/src/app/shared/brightness-control/brightness-control.component.ts
+++ apps/synapse-copycat/src/app/shared/brightness-control/brightness-control.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SliderComponent, ToggleComponent } from '@synapse/ui'
+
+
 @Component({
   selector: 'synapse-copycat-brightness-control',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, 
+    SliderComponent,
+    ToggleComponent
+  ],
   templateUrl: './brightness-control.component.html',
   styleUrl: './brightness-control.component.scss',
 })
