36a0c99dbbebea2bf54277f5161fae588f3db1ad
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-18 16:05:55 -0500

    feat(synapse-copycat): update mousemat page

--- apps/synapse-copycat/src/app/pages/mousemat-page/mousemat-page.component.ts
+++ apps/synapse-copycat/src/app/pages/mousemat-page/mousemat-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrightnessControlComponent } from '../../shared/brightness-control/brightness-control.component';
+import { LightingControlComponent } from '../../shared/lighting-control/lighting-control.component';
 
 @Component({
   selector: 'synapse-copycat-mousemat-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    BrightnessControlComponent,
+    LightingControlComponent,
+  ],
   templateUrl: './mousemat-page.component.html',
   styleUrl: './mousemat-page.component.scss',
 })
