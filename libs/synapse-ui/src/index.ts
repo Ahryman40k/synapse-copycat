9e9940d03acc037c326e21d1f349c057c9cf424e
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-18 16:05:07 -0500

    feat(synapse-ui): update checkbox component

--- libs/synapse-ui/src/index.ts
+++ libs/synapse-ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/toggle/toggle.component';
 export * from './lib/slider/slider.component';
 export * from './lib/button/button.component';
+export * from './lib/checkbox/checkbox.component';
