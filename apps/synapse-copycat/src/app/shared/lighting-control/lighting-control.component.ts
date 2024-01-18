842732d5e4ffa414d0dd93e1a9f699ebe6d77624
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-18 16:03:57 -0500

    feat(synapse-copycat): create lighting control component

---
+++ apps/synapse-copycat/src/app/shared/lighting-control/lighting-control.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CheckboxComponent } from '@synapse/ui'
+
+@Component({
+  selector: 'synapse-copycat-lighting-control',
+  standalone: true,
+  imports: [CommonModule, CheckboxComponent],
+  templateUrl: './lighting-control.component.html',
+  styleUrl: './lighting-control.component.scss',
+})
+export class LightingControlComponent {}
