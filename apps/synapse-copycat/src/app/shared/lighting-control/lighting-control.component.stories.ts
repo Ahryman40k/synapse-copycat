02a2b80cb785d0d3dc17a816773475dbf0a3f483
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-19 14:17:07 -0500

    test(angular-copycat): add stories for lighting control

---
+++ apps/synapse-copycat/src/app/shared/lighting-control/lighting-control.component.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/angular';
+
+import { LightingControlComponent } from './lighting-control.component';
+
+const meta: Meta<LightingControlComponent> = {
+  title: 'Components / Device Controls / Lighting',
+  component: LightingControlComponent,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<LightingControlComponent>;
+
+export const Control: Story = {
+};
+
