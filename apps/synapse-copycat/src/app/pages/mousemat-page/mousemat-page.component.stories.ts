95972777d41c62b4c299f489fa9d84cfec189c6c
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-19 14:43:10 -0500

    feat(synapse-copycat): update mousemat page

---
+++ apps/synapse-copycat/src/app/pages/mousemat-page/mousemat-page.component.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/angular';
+
+import { MousematPageComponent } from './mousemat-page.component';
+
+const meta: Meta<MousematPageComponent> = {
+  title: 'Pages / Mousemat Page',
+  component: MousematPageComponent,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<MousematPageComponent>;
+
+export const DefaultPage: Story = {
+};
+
