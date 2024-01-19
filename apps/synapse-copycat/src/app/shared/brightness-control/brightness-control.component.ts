aea0ed13a7487811ff8eb0265efaaeea47b9c264
Geoffrey Baudin <geoffrey.baudin.ext@montreal.ca>
2024-01-19 13:42:12 -0500

    feat(angular-copycat): update brightness control behaviour

--- apps/synapse-copycat/src/app/shared/brightness-control/brightness-control.component.ts
+++ apps/synapse-copycat/src/app/shared/brightness-control/brightness-control.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SliderComponent, ToggleComponent } from '@synapse/ui'
@@ -14,4 +14,11 @@ import { SliderComponent, ToggleComponent } from '@synapse/ui'
   templateUrl: './brightness-control.component.html',
   styleUrl: './brightness-control.component.scss',
 })
-export class BrightnessControlComponent {}
+export class BrightnessControlComponent {
+
+  @ViewChild('slider') slider!: SliderComponent
+
+  toggleChanged( state: boolean) {
+    this.slider.disabled = state
+  }
+}
