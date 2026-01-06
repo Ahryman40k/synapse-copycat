a37221185ae37a2a54d9056c50934b7e49ba1aa4
ahryman <ahryman40k.gb@gmail.com>
2026-01-05 23:34:00 -0500

    feat(synapse):add mousemat page

---
+++ apps/synapse/src/app/domains/devices/mousemat-page/mousemat-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mousemat-page',
+  templateUrl: './mousemat-page.html',
+  styleUrl: './mousemat-page.scss',
+})
+export class MousematPageComponent { }
