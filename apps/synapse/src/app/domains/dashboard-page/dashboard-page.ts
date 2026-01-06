225e97fa649678d89b50c2dafd51ba67fb1f3ef3
ahryman <ahryman40k.gb@gmail.com>
2026-01-05 23:38:20 -0500

    feat(synapse):update dashboard page

---
+++ apps/synapse/src/app/domains/dashboard-page/dashboard-page.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ApplicationStore } from '../../core/stores/application-store';
+
+@Component({
+  selector: 'dashboard-page',
+  templateUrl: './dashboard-page.html',
+  styleUrl: './dashboard-page.scss',
+  imports: [CommonModule],
+})
+export class DashboardPage {
+  readonly #store = inject(ApplicationStore);
+
+  protected devices = this.#store.devices;
+  protected modules = this.#store.modules;
+}
