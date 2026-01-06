3e8d5657ea296088b3e7b70157bb90aeebced6a3
ahryman <ahryman40k.gb@gmail.com>
2026-01-05 23:30:13 -0500

    feat: add UI/switch component

---
+++ libs/ui/src/lib/switch/switch.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { SwitchComponent } from './switch';
+
+const meta: Meta<SwitchComponent> = {
+  component: SwitchComponent,
+  title: 'UI library / Switch',
+};
+
+export default meta;
+type Story = StoryObj<SwitchComponent>;
+
+export const Default: Story = {
+  args: {},
+  // render: () => ({
+  //   template: `<syn-switch></syn-switch>`,
+  // }),
+};
