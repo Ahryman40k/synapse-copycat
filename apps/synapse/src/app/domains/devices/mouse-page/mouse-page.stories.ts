2c0488646a6fca9335359e62c0012ffa8e26a9b1
ahryman <ahryman40k.gb@gmail.com>
2026-01-05 23:37:57 -0500

    feat(synapse):add mouse page

---
+++ apps/synapse/src/app/domains/devices/mouse-page/mouse-page.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { MousePageComponent } from './mouse-page';
+
+const meta: Meta<MousePageComponent> = {
+  component: MousePageComponent,
+  title: 'Synapse Application / Pages / Mouse',
+};
+export default meta;
+
+type Story = StoryObj<MousePageComponent>;
+
+export const Default: Story = {};
---
+++ apps/synapse/src/app/domains/devices/mouse-page/mouse-page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import {
+  type DescriptorItem,
+  PageBarComponent,
+  type PageBarDescriptor,
+} from '../../page-bar/page-bar';
+import { MouseLightingPanelComponent } from './lighting/mouse-lighting';
+import { NgComponentOutlet } from '@angular/common';
+import { MouseCustomizePanelComponent } from './customize/mouse-customize';
+
+@Component({
+  selector: 'mouse-page',
+  styleUrl: './mouse-page.scss',
+  templateUrl: './mouse-page.html',
+  imports: [PageBarComponent, NgComponentOutlet],
+})
+export class MousePageComponent {
+  descriptor: PageBarDescriptor = [
+    {
+      title: 'customize',
+      component: MouseCustomizePanelComponent,
+    },
+    {
+      title: 'performance',
+      component: MouseLightingPanelComponent,
+    },
+    {
+      title: 'lighting',
+      component: MouseLightingPanelComponent,
+    },
+    {
+      title: 'calibration',
+      component: MouseLightingPanelComponent,
+    },
+    {
+      title: 'power',
+      component: MouseLightingPanelComponent,
+    },
+  ];
+
+  selectedPanel = this.descriptor[0];
+
+  onPanelChanging(item: DescriptorItem): void {
+    this.selectedPanel = item;
+  }
+}
