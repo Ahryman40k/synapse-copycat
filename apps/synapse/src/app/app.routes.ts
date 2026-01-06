778603a7b2310212e06b42115377c869d9bbbb39
ahryman <ahryman40k.gb@gmail.com>
2026-01-05 23:39:15 -0500

    feat(synapse):activate route

--- apps/synapse/src/app/app.routes.ts
+++ apps/synapse/src/app/app.routes.ts
@@ -1,32 +1,56 @@
-import type { Route } from '@angular/router';
-import { DashboardPage } from './domains/dashboard-page';
+import type { ResolveFn, Route } from '@angular/router';
+import { DashboardPage } from './domains/dashboard-page/dashboard-page';
+import { ApplicationStore } from './core/stores/application-store';
+import { inject } from '@angular/core';
+import type { Device, Module } from '@synapse-copycat/backend-api';
+import { MousePageComponent } from './domains/devices/mouse-page/mouse-page';
+import { MousematPageComponent } from './domains/devices/mousemat-page/mousemat-page';
+
+export const devicesResolver: ResolveFn<Device[]> = () => {
+  const store = inject(ApplicationStore);
+  store.getDevices();
+  return store.devices();
+};
+
+export const modulesResolver: ResolveFn<Module[]> = () => {
+  const store = inject(ApplicationStore);
+  store.getModules();
+  return store.modules();
+};
 
 export const appRoutes: Route[] = [
-	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-	{ path: 'dashboard', component: DashboardPage },
-	// {
-	//   path: 'device',
-	//   children: [
-	//     {
-	//       path: 'mousemat',
-	//       component: MousematPageComponent,
-	//     },
-	//     {
-	//       path: 'mouse',
-	//       component: MousePageComponent,
-	//     },
-	//     {
-	//       path: 'keyboard',
-	//       component: KeyboardPageComponent,
-	//     },
-	//     {
-	//       path: 'accessory',
-	//       component: AccessoryPageComponent,
-	//     },
-	//     {
-	//       path: 'streaming',
-	//       component: StreamingPageComponent,
-	//     },
-	//   ],
-	// },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    component: DashboardPage,
+    resolve: {
+      devices: devicesResolver,
+      modules: modulesResolver,
+    },
+  },
+  {
+    path: 'device',
+    children: [
+      {
+        path: 'mousemat',
+        component: MousematPageComponent,
+      },
+      {
+        path: 'mouse',
+        component: MousePageComponent,
+      },
+      // {
+      //   path: 'keyboard',
+      //   component: KeyboardPageComponent,
+      // },
+      // {
+      //   path: 'accessory',
+      //   component: AccessoryPageComponent,
+      // },
+      // {
+      //   path: 'streaming',
+      //   component: StreamingPageComponent,
+      // },
+    ],
+  },
 ];
