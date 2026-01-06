fe44e78082364c19e75e33f5e1acede773d9bbe0
ahryman <ahryman40k.gb@gmail.com>
2026-01-05 23:36:34 -0500

    feat(synapse):add lighting switch off panel

---
+++ apps/synapse/src/app/core/components/lighting-switch-off-panel/lighting-switch-off-panel.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { LightingSwitchOffPanelComponent } from './lighting-switch-off-panel';
+
+const meta: Meta<LightingSwitchOffPanelComponent> = {
+  component: LightingSwitchOffPanelComponent,
+  title: 'Synapse application / Components / lighting switch-off panel',
+};
+
+export default meta;
+type Story = StoryObj<LightingSwitchOffPanelComponent>;
+
+export const Default: Story = {
+  name: 'Default Panel',
+};
---
+++ apps/synapse/src/app/core/components/lighting-switch-off-panel/lighting-switch-off-panel.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Card, CheckboxComponent } from '@synapse-copycat/ui';
+
+@Component({
+  selector: 'lighting-switch-off-panel',
+  templateUrl: './lighting-switch-off-panel.html',
+  styleUrl: './lighting-switch-off-panel.scss',
+  imports: [CheckboxComponent, Card],
+})
+export class LightingSwitchOffPanelComponent { }
